2c7bba51585e8fc74d86c61a889006513fd9c88c
Alejandro Guariguata <tabebuia@protonmail.com>
2023-02-04 20:19:43 -0600

    added s3 updating, changed update script

--- update.sh
+++ update.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-rm -rf dist/
-mkdir dist/
-mkdir dist/coins/
 python3 crypto51/app.py
 python3 crypto51/render.py
-if [ "$1" != "noprompt" ]; then
-    read -p "Please check the output and then press [ENTER] to continue"
-fi
-# gsutil defacl set public-read gs://www.crypto51.app
-# gsutil defacl set public-read gs://api.crypto51.app
-gsutil -h "Cache-Control:public, max-age=600" -m rsync -d -r dist/ gs://www.crypto51.app/
-gsutil -h "Cache-Control:public, max-age=600" cp dist/coins.json gs://api.crypto51.app/
-gsutil -h "Cache-Control:public, max-age=600" cp dist/coins.json gs://api.crypto51.app/history/`date +%Y/%m/%d/%H-%M`.json
+python3 crypto51/s3update.py
